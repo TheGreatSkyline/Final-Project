bcd124fd8d12fc41bfb4c8abdd44d57ae11b420b
Joaquin Gomez <joaquingomez@my.whccd.edu>
2024-11-30 18:16:37 -0800

    setting up the game

--- script.js
+++ script.js
@@ -6,3 +6,20 @@
 * This is a TIC TAC TOE game
 */
 
+// Get references to DOM elements
+// Set ups the game
+// the game board
+const squares = document.querySelectorAll('.game-square');
+// the turn tracker
+const turnDisplay = document.getElementById('turn');
+// the play again button
+const playAgainButton = document.getElementById('button-play-again');
+// the scoreboard for each player
+const scoreboardX = document.getElementById('scoreboard-x');
+// how many times player X has won
+const scoreboardO = document.getElementById('scoreboard-o');
+// how many times player O has won
+// if the game is a tie
+const scoreboardTies = document.getElementById('scoreboard-ties'); // New element for ties
+
+
